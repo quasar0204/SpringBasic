80dd0369b4d2ac9cb4bc07f2daf27e2ea71607a8
quasar0204 <quasar13245@gmail.com>
2021-04-16 23:30:20 +0900

    애플리케이션 빈만 출력하기

--- core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -19,4 +19,18 @@ public class ApplicationContextInfoTest {
             System.out.println("name = " + beanDefinitionName + " object = " + bean);
         }
     }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
 }
